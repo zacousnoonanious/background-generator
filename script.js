ee1eb62561e7557e35509550d1162d8ba715566e
zac noonan <znoonan@uaa.alaska.edu>
2018-05-15 15:10:40 -0800

    added initial starting project

---
+++ script.js
@@ -0,0 +1,18 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+console.log("color1 " + color1.value);
+color1.value = "#11ff11";
+color2.value = "#110000";
+
+function setGradient() {
+  body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  css.textContent = body.style.background;
+}
+
+setGradient();
+
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
